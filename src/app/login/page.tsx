f8c8b9123549ef2791c8a3d5a5f39ae01465a328
liem754 <pliem754@gmail.com>
2023-11-05 11:14:21 +0700

    newwww

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,14 +9,18 @@ function Login() {
   const dispatch = useDispatch();
   const route = useRouter();
   const [isRegister, setIsRegister] = React.useState(false);
+  const [load, setLoad] = React.useState(false);
+
   const [payload, setPayload] = React.useState({
     email: "",
     password: "",
   });
   const handle = async () => {
+    setLoad(true);
     const final = { email: payload.email, password: payload.password };
     const rss = await login(final);
     if (rss.data.err === 0) {
+      setLoad(false);
       dispatch(
         logintoken({
           token: rss.data.access_token,
@@ -24,6 +28,7 @@ function Login() {
       );
       route.push("/");
     } else {
+      setLoad(false);
       Swal.fire("Oops!", rss.data.mes, "error");
     }
   };
@@ -63,7 +68,15 @@ function Login() {
           onClick={handle}
           className=" mt-7  outline-none border-none text-white bg-blue-600 hover:bg-blue-500 py-2"
         >
-          Login
+          {load ? (
+            <div className="flex flex-row gap-2 py-2 px-4 ">
+              <div className="w-2 h-2 rounded-full bg-white animate-bounce [animation-delay:.7s]"></div>
+              <div className="w-2 h-2 rounded-full bg-white animate-bounce [animation-delay:.3s]"></div>
+              <div className="w-2 h-2 rounded-full bg-white animate-bounce [animation-delay:.7s]"></div>
+            </div>
+          ) : (
+            <p> Create</p>
+          )}
         </button>
         <h2
           className=" cursor-pointer hover:text-yellow-600 text-ellipsis"
